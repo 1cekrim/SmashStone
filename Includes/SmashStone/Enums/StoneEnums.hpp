f0bc50330e23392a8aef08e0806809f09de64139
HyeonSu Kim <rla92233@naver.com>
2019-07-21 13:27:16 +0900

    impl: Implement 'StoneEnums'

---
+++ Includes/SmashStone/Enums/StoneEnums.hpp
@@ -0,0 +1,15 @@
+#ifndef SMASHSTONE_STONEENUMS_HPP
+#define SMASHSTONE_STONEENUMS_HPP
+
+namespace SmashStone
+{
+enum class StoneColor
+{
+    BLACK = 0,
+    WHITE,
+    COUNT,
+    INVALID,
+};
+}
+
+#endif  // SMASHSTONE_STONEENUMS_HPP

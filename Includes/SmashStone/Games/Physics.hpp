88ddde319321574a6418ee12273188c197369d78
HyeonSu Kim <rla92233@naver.com>
2019-07-21 21:39:53 +0900

    impl: Implement 'Physics'

---
+++ Includes/SmashStone/Games/Physics.hpp
@@ -0,0 +1,48 @@
+#ifndef SMASHSTONE_GAMES_PHYSICS_HPP
+#define SMASHSTONE_GAMES_PHYSICS_HPP
+
+#include <SmashStone/Enums/StoneEnums.hpp>
+#include <SmashStone/Utils/Vector2D.hpp>
+#include <SmashStone/Models/Stone.hpp>
+
+#include <vector>
+#include <map>
+
+namespace SmashStone::Games
+{
+struct Manifold
+{
+    Models::Stone* source;
+    Models::Stone* target;
+    float penetration;
+    Utils::Vector2D<float> normal;
+
+    Manifold(Models::Stone& source, Models::Stone& target) : source(&source), target(&target), penetration(0.0f), normal(0.0f, 0.0f)
+    {
+        // Do nothing
+    }
+};
+
+class Physics
+{   
+public:
+    Physics() = delete;
+    Physics(const float& friction, const float& elasticity);
+
+    void Update(const float& dt);
+    void AffectFriction(const float& dt);
+    void UpdatePosition(const float& dt);
+    void CheckCrash(void);
+    const bool UpdateManifold(Manifold& manifold) const;
+
+    void DoCrash(Manifold& manifold) const;
+
+    void DeleteStone(const int& id);
+    const int AddStone(Models::Stone& stone);
+
+    float friction, elasticity;
+    std::vector<Manifold> manifolds;
+    std::map<StoneColor, std::vector<Models::Stone>> stones;
+};
+}  // namespace SmashStone::Games
+#endif  // SMASHSTONE_GAMES_PHYSICS_HPP
---
+++ Source/SmashStone/Games/Physics.cc
@@ -0,0 +1,137 @@
+#include <SmashStone/Enums/StoneEnums.hpp>
+#include <SmashStone/Games/Physics.hpp>
+#include <SmashStone/Models/Stone.hpp>
+
+namespace SmashStone::Games
+{
+Physics::Physics(const float& friction, const float& elasticity)
+    : friction(friction), elasticity(elasticity)
+{
+    stones.emplace(std::piecewise_construct, std::make_tuple(StoneColor::BLACK), std::make_tuple());
+    stones.emplace(std::piecewise_construct, std::make_tuple(StoneColor::WHITE), std::make_tuple());
+}
+
+void Physics::Update(const float& dt)
+{
+    //change velocity functions
+    AffectFriction(dt);
+
+    //change position functions
+    UpdatePosition(dt);
+
+    //check crash functions
+    CheckCrash();
+}
+
+void Physics::AffectFriction(const float& dt)
+{
+    std::vector<Models::Stone>& blackStones = stones.at(StoneColor::BLACK);
+    std::vector<Models::Stone>& whiteStones = stones.at(StoneColor::WHITE);
+
+    for (Models::Stone& bs : blackStones)
+    {
+        bs.velocity -= friction * bs.velocity.Normalized() * dt;
+    }
+
+    for (Models::Stone& ws : whiteStones)
+    {
+        ws.velocity -= friction * ws.velocity.Normalized() * dt;
+    }
+}
+
+void Physics::UpdatePosition(const float& dt)
+{
+    std::vector<Models::Stone>& blackStones = stones.at(StoneColor::BLACK);
+    std::vector<Models::Stone>& whiteStones = stones.at(StoneColor::WHITE);
+
+    for (Models::Stone& bs : blackStones)
+    {
+        bs.position += bs.velocity * dt;
+    }
+
+    for (Models::Stone& ws : whiteStones)
+    {
+        ws.position += ws.velocity * dt;
+    }
+}
+
+const int Physics::AddStone(Models::Stone& stone)
+{
+    std::vector<Models::Stone>& blackStones = stones.at(StoneColor::BLACK);
+    std::vector<Models::Stone>& whiteStones = stones.at(StoneColor::WHITE);
+    std::vector<Models::Stone>& sameColorStones = stones.at(stone.color);
+
+    for (Models::Stone& bs : blackStones)
+    {
+        manifolds.emplace_back(stone, bs);
+    }
+
+    for (Models::Stone& ws : whiteStones)
+    {
+        manifolds.emplace_back(stone, ws);
+    }
+
+    sameColorStones.emplace_back(stone);
+    //CheckCrash();
+    return sameColorStones.size() - 1;
+}
+
+void Physics::CheckCrash(void)
+{
+    for (Manifold& manifold : manifolds)
+    {
+        if (UpdateManifold(manifold))
+        {
+            DoCrash(manifold);
+        }
+    }
+}
+
+const bool Physics::UpdateManifold(Manifold& manifold) const
+{
+    Models::Stone* source = manifold.source;
+    Models::Stone* target = manifold.target;
+
+    Utils::Vector2D<float> dir = target->position - source->position;
+
+    const float d = dir.Norm(2);
+    const float r = source->radius + target->radius;
+
+    if (r < d)
+    {
+        return false;
+    }
+
+    if (d != 0)
+    {
+        manifold.penetration = r - d;
+        manifold.normal = dir / d;
+        
+        return true;
+    }
+    else
+    {
+        manifold.penetration = source->radius;
+        manifold.normal = Utils::Vector2D<float>(1.0f, 0.0f);
+        return true;
+    }
+}
+
+void Physics::DoCrash(Manifold& manifold) const
+{
+    Utils::Vector2D<float> rv = manifold.target->position - manifold.source->position;
+
+    const float vn = rv.Dot(manifold.normal);
+    if (vn > 0)
+    {
+        return;
+    }
+
+    const float j = -1 * (1 + elasticity) * vn / (manifold.source->invMass + manifold.target->invMass);
+
+    const Utils::Vector2D<float> impulse = j * manifold.normal;
+
+    manifold.source->velocity -= manifold.source->invMass * impulse;
+    manifold.target->velocity -= manifold.target->invMass * impulse;
+}
+}

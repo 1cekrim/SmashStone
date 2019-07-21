858be314d9032874de6b9dc5e752e65e8ac39b82
HyeonSu Kim <rla92233@naver.com>
2019-07-21 13:30:21 +0900

    impl: Implement 'Stone'

---
+++ Includes/SmashStone/Models/Stone.hpp
@@ -0,0 +1,22 @@
+#ifndef SMASHSTONE_MODELS_STONE_HPP
+#define SMASHSTONE_MODELS_STONE_HPP
+
+#include <SmashStone/Enums/StoneEnums.hpp>
+#include <SmashStone/Utils/Vector2D.hpp>
+
+namespace SmashStone::Models
+{
+class Stone
+{
+    Stone() = delete;
+    Stone(StoneColor& color, float& mass, float& radius, Utils::Vector2D<float>& position, Utils::Vector2D<float>& velocity);
+
+    void AddForce(Utils::Vector2D<float>& force) const;
+
+    StoneColor color;
+    int id;
+    float mass, radius, invMass;
+    Utils::Vector2D<float> position, velocity;
+};
+}  // namespace SmashStone::Models
+#endif  // SMASHSTONE_MODELS_STONE_HPP
---
+++ Source/SmashStone/Models/Stone.cc
@@ -0,0 +1,10 @@
+#include <SmashStone/Models/Stone.hpp>
+
+namespace SmashStone::Models
+{
+Stone::Stone(StoneColor& color, float& mass, float& radius, Utils::Vector2D<float>& position, Utils::Vector2D<float>& velocity)
+: color(color), mass(mass), invMass(1 / mass), radius(radius), position(position), velocity(velocity)
+{
+    // Do nothing
+}
+}  // namespace SmashStone::Models

99d5bbe19faa6727cb292e591635c6a1e1383b2e
HyeonSu Kim <rla92233@naver.com>
2019-07-22 16:59:47 +0900

    impl: Add IPlayer

---
+++ Includes/SmashStone/Models/IPlayer.hpp
@@ -0,0 +1,28 @@
+#ifndef SMASHSTONE_MODELS_IPLAYER_HPP
+#define SMASHSTONE_MODELS_IPLAYER_HPP
+
+#include <SmashStone/Enums/StoneEnums.hpp>
+#include <SmashStone/Enums/GameEnums.hpp>
+#include <SmashStone/Utils/Vector2D.hpp>
+
+#include <functional>
+
+namespace SmashStone::Models
+{
+struct Action
+{
+    int SelectedStoneIdx;
+    Utils::Vector2D<float> velocity;
+};
+class IPlayer
+{
+public:
+    virtual void Ready(void) = 0; 
+    virtual Action GetAction(void) = 0;
+    virtual void TurnEnd(bool isMyTurn, TurnResult result) = 0;
+    std::function<bool (std::vector<Utils::Vector2D<float>>)> PutStones;
+    std::function<std::vector<Utils::Vector2D<float>> ()> GetMyStones;
+    std::function<std::vector<Utils::Vector2D<float>> ()> GetOpStones;
+};
+}  // namespace SmashStone::Models
+#endif  // SMASHSTONE_MODELS_IPLAYER_HPP

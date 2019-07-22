3a6fbdd28d7e13898d415a56f814d9f1d7a20f00
HyeonSu Kim <rla92233@naver.com>
2019-07-22 16:59:10 +0900

    impl: Add basic fuctions(incomplete)

--- Includes/SmashStone/Games/Board.hpp
+++ Includes/SmashStone/Games/Board.hpp
@@ -4,6 +4,7 @@
 #include <SmashStone/Enums/StoneEnums.hpp>
 #include <SmashStone/Utils/Vector2D.hpp>
 #include <SmashStone/Games/Physics.hpp>
+#include <SmashStone/Models/IPlayer.hpp>
 
 #include <vector>
 
@@ -12,16 +13,30 @@ namespace SmashStone::Games
 class Board
 {
 public:
-    Board() = delete;
+    Board();
     Board(const float& height, const float& width);
+    ~Board();
 
-    void CheckOutBoundStone(void);
+    void InitBoard(const float& friction, const float& elasticity);
 
-    const int GetStoneCount(const StoneColor& color) const;
-    std::vector<Models::Stone> GetStones(const StoneColor& color) const;
+    bool PutStones(StoneColor color, std::vector<Utils::Vector2D<float>> positions);
 
+    bool CanPutHere(Utils::Vector2D<float> pos);
+
+    std::vector<Models::Stone> GetStones(StoneColor color) const;
+    template<class T>
+    void SetPlayer(const int& playerNumber);
+
+    void PlayerReady(const int& playerNumber);
+    void PlayerDoAction(const int& playerNumber);
+    void DoActionAndWait(const int& playerNumber, Models::Action action);
+    void DestroyOutBoundStone(void);
+    bool CheckGameEnd(void);
+    
     const float height, width;
-    Physics* physics;
+    std::unique_ptr<Physics> physics;
+    std::unique_ptr<Models::IPlayer> player1;
+    std::unique_ptr<Models::IPlayer> player2;
 };
 }  // namespace SmashStone::Games
 #endif  // SMASHSTONE_GAMES_BOARDS_HPP
--- Source/SmashStone/Games/Board.cc
+++ Source/SmashStone/Games/Board.cc
@@ -1,21 +1,136 @@
 #include <SmashStone/Games/Board.hpp>
-
+#include <memory>
+#include <iostream>
 namespace SmashStone::Games
 {
 Board::Board(const float& height, const float& width) : height(height), width(width)
 {
-    // pass
+    // Do nothing
 }
-void Board::CheckOutBoundStone(void)
+
+Board::Board() : Board(0.45f, 0.42f)
 {
-    // pass
+    // Do nothing
 }
-const int Board::GetStoneCount(const StoneColor& color) const
+
+void Board::InitBoard(const float& friction, const float& elasticity)
 {
-    // pass
+    physics = std::make_unique<Physics>(friction, elasticity);
 }
-std::vector<Models::Stone> Board::GetStones(const StoneColor& color) const
+
+bool Board::PutStones(StoneColor color, std::vector<Utils::Vector2D<float>> positions)
 {
-    //pass
+    for (auto pos : positions)
+    {
+        if (!CanPutHere(pos))
+        {
+            return false;
+        }
+        physics.get()->AddStone(color, 0.041, 0.011, pos, Utils::Vector2D<float>(0, 0));
+    }
+    return true;
+}
+
+bool Board::CanPutHere(Utils::Vector2D<float> pos)
+{
+    if (pos.x_ < 0 || pos.y_ < 0 || pos.x_ > width || pos.y_ > height)
+    {
+        return false;
+    }
+
+    for (auto& stone : physics.get()->stones.at(StoneColor::BLACK))
+    {
+        const float r = stone.radius * 2;
+        const float d = (pos - stone.position).Norm(2);
+
+        if (d < r)
+        {
+            return false;
+        }
+    }
+
+    for (auto& stone : physics.get()->stones.at(StoneColor::WHITE))
+    {
+        const float r = stone.radius * 2;
+        const float d = (pos - stone.position).Norm(2);
+
+        if (d < r)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+std::function<bool (std::vector<Utils::Vector2D<float>>)> PutStones;
+template<class T>
+void Board::SetPlayer(const int& playerNumber)
+{
+    switch (playerNumber)
+    {
+        case 1:
+            player1 = std::make_unique<T>();
+            player1.get()->GetMyStones = std::bind(GetStones, StoneColor::BLACK);
+            player1.get()->GetOpStones = std::bind(GetStones, StoneColor::WHITE);
+            player1.get()->PutStones = std::bind(PutStones, StoneColor::BLACK, std::placeholders::_1);
+            break;
+        case 2:
+            player2 = std::make_unique<T>();
+            player2.get()->GetMyStones = std::bind(GetStones, StoneColor::WHITE);
+            player2.get()->GetOpStones = std::bind(GetStones, StoneColor::BLACK);
+            player2.get()->PutStones = std::bind(PutStones, StoneColor::WHITE, std::placeholders::_1);
+            break;
+        default:
+            break;
+    }
+}
+
+void Board::PlayerReady(const int& playerNumber)
+{
+    switch (playerNumber)
+    {
+    case 1:
+        player1.get()->Ready();
+        break;
+    case 2:
+        player2.get()->Ready();
+        break;
+    default:
+        break;
+    }
+}
+
+void Board::PlayerDoAction(const int& playerNumber)
+{
+    switch (playerNumber)
+    {
+    case 1:
+        break;
+    case 2:
+        break;
+    default:
+        break;
+    }
+}
+
+void Board::DoActionAndWait(const int& playerNumber, Models::Action action)
+{
+
+}
+
+void Board::DestroyOutBoundStone(void)
+{
+
+}
+
+bool Board::CheckGameEnd(void)
+{
+
+}
+
+std::vector<Models::Stone> Board::GetStones(StoneColor color) const
+{
+    
 }
 }

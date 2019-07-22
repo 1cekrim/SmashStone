9608c5a56486f532a5a3390e563c17eea965bc56
HyeonSu Kim <rla92233@naver.com>
2019-07-22 16:57:21 +0900

    impl: Add TurnResult enum

---
+++ Includes/SmashStone/Enums/GameEnums.hpp
@@ -0,0 +1,15 @@
+#ifndef SMASHSTONE_GAMEENUMS_HPP
+#define SMASHSTONE_GAMEENUMS_HPP
+
+namespace SmashStone
+{
+enum class TurnResult
+{
+    WIN,
+    LOSE,
+    DRAW,
+    NOPE,
+};
+}
+
+#endif  // SMASHSTONE_GAMEENUMS_HPP
